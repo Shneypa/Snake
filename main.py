d11f476e76e9c23046d724cc46fd7f435617df42
Shneypa Kalashnikov <osinfedor@gmail.com>
2015-10-13 23:55:47 +0300

    Snake and apple sprites

--- main.py
+++ main.py
@@ -23,33 +23,98 @@ pygame.display.set_caption("Slither")
 
 
 
-block_size = WINDOW_WIDTH / 80 * 4                                                 # snake moves 10 pixels per frame
+block_size = 20                                          # snake moves 20 pixels per frame
 appleThickness = 30
 
-clock = pygame.time.Clock()                                             # variable to control FPS
-FPS = 15
+img = pygame.image.load('snake_head.png')                # load snake head spirte
+apple_img = pygame.image.load('strawberry.png')          # load apple sprite
+
+clock = pygame.time.Clock()                              # variable to control FPS
+FPS = 30
+
+starting_direction = "right"
+direction = starting_direction
+
+smallfont = pygame.font.SysFont("comicsansms", 25)
+medfont = pygame.font.SysFont("comicsansms", 50)
+largefont = pygame.font.SysFont("comicsansms", 80)
 
 
+# Intro Loop
 
-# Putting messages to screen (will put in the middle of the scrren by default)
+def game_intro():
 
-font_size = 25
-font = pygame.font.SysFont(None, font_size)
+    intro = True
 
-def snake(block_size, snakelist):
-    for XnY in snakelist:
+    while intro:
+        gameDisplay.fill(white)
+        message_to_screen("Welcome to Slither", green, -100, font_size= "large")
+
+        message_to_screen("The objective of the game is to read red apples", black, -30, "small")
+        message_to_screen("The more apples you eat the longer you get.", black, 0, "small")
+        message_to_screen("If you run into yourself, you die!", black, 30, "small")
+        message_to_screen("Press C to play or Q to quit.", black, 180, "small")
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    intro = False
+                elif event.key == pygame.K_q:
+                    pygame.quit()
+                    quit()
+
+        pygame.display.update()
+        clock.tick(15)
+
+
+# DRAWING THE SNAKE
+
+def draw_snake(block_size, snakelist):
+
+    # rotation of snake head sprite
+
+    if direction == "right":
+        head = pygame.transform.rotate(img, 270)
+    elif direction == "left":
+        head = pygame.transform.rotate(img, 90)
+    elif direction == "up":
+        head = img
+    elif direction == "down":
+        head = pygame.transform.rotate(img, 180)
+
+
+    # drawing snake head
+
+    gameDisplay.blit(head, (snakelist[-1][0], snakelist[-1][1]))            # draw snake head using sprite
+
+    # drawing snake segments
+
+    for XnY in snakelist[:-1]:                                           # draw all other snake segments
         pygame.draw.rect(gameDisplay, green, [XnY[0],XnY[1],block_size,block_size])         # (x,y,width,height)
 
-def text_objects(text, color):
-    textSurface = font.render(text, True, color)
+
+
+
+def text_objects(text, color, font_size):
+    if font_size == "small":
+        textSurface = smallfont.render(text, True, color)
+    elif font_size == "medium":
+        textSurface = medfont.render(text, True, color)
+    elif font_size == "large":
+        textSurface = largefont.render(text, True, color)
     return textSurface, textSurface.get_rect()
 
-def message_to_screen(msg, color):
-    textSurf, textRect = text_objects(msg, color)
-    textRect.center = (WINDOW_WIDTH /2), (WINDOW_HEIGHT /2 )
+def message_to_screen(msg, color = black, y_displacement = 0, font_size = "small"):                  # discplacement from the center on Y axis
+
+    textSurf, textRect = text_objects(msg, color, font_size)
+    textRect.center = (WINDOW_WIDTH /2), (WINDOW_HEIGHT /2 + y_displacement)
     gameDisplay.blit(textSurf, textRect)
-    #screen_text = font.render(msg, True, color)
-    #gameDisplay.blit(screen_text, [(WINDOW_WIDTH / 2, WINDOW_HEIGHT/2])
+
 
 def randX():
        return (random.randrange((WINDOW_WIDTH - appleThickness) // block_size)) * block_size
@@ -64,6 +129,9 @@ def randY():
 # GAME LOOP
 def gameLoop():
 
+    global direction        # now we can modify the direction variable which is initialized outside game loop
+    direction = starting_direction
+
     # Game variables
 
     gameExit = False
@@ -71,7 +139,7 @@ def gameLoop():
 
     lead_x = WINDOW_WIDTH / 2
     lead_y = WINDOW_HEIGHT / 2
-    lead_x_change = 0
+    lead_x_change = block_size
     lead_y_change = 0
 
     apple_x = randX()
@@ -80,19 +148,20 @@ def gameLoop():
     snakelist = []
     snakelength = 1
 
-    snake_out_of_bounds = False
-    snake_on_apple = False
-
 
     # GAME LOOP:
 
     while not gameExit:
 
+
     # Play again or quit?
 
         while gameOver == True:
+
             gameDisplay.fill(white)
-            message_to_screen("Game over, press C to play again or Q to quit", red)
+            message_to_screen("Game over",  y_displacement = -50, color = red, font_size = "large")
+            message_to_screen("Press C to play again or Q to quit",black, 50, font_size = "small")
+
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -119,22 +188,26 @@ def gameLoop():
 
             if event.type is pygame.KEYDOWN:
 
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                       if lead_x_change <= 0:        # if we were going right, disallow instant reverse of direction (so not to crash in itself)
                           lead_x_change = -block_size     # move 10 pixels left at every update
                           lead_y_change = 0         # reset vertical movement
-                elif event.key == pygame.K_RIGHT:
+                          direction = "left"
+                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                       if lead_x_change >= 0:
                           lead_x_change = block_size
                           lead_y_change = 0
-                elif event.key == pygame.K_UP:
+                          direction = "right"
+                elif event.key == pygame.K_UP or event.key == pygame.K_w:
                       if lead_y_change <= 0:
                           lead_y_change = -block_size
                           lead_x_change = 0
-                elif event.key == pygame.K_DOWN:
+                          direction = "up"
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                       if lead_y_change >= 0:
                           lead_y_change = block_size
                           lead_x_change = 0
+                          direction = "down"
 
 
         # UPDATE AND RENDER
@@ -145,18 +218,19 @@ def gameLoop():
         # check if snake crashed into itself (any segment except head is equal to head)
         for eachSegment in snakelist[:-1]:
             if eachSegment in snakehead:
+                print("crashed into self!")
                 gameOver
+
 
         # check if snake ran out of bounds
         if lead_x >= WINDOW_WIDTH or lead_x < 0 or lead_y < 0 or lead_y >= WINDOW_HEIGHT:
-            snake_out_of_bounds = True
             gameOver = True
+
 
         gameDisplay.fill(white)                                                 # draws 'in the background'. we clean the screen.
 
-
-        pygame.draw.rect(gameDisplay, red, [apple_x, apple_y, appleThickness, appleThickness])
-
+        # drawing apple
+        gameDisplay.blit(apple_img, (apple_x,apple_y))
 
         snakehead = []
         snakehead.append(lead_x)
@@ -166,7 +240,8 @@ def gameLoop():
         if len(snakelist) > snakelength:
             del snakelist[0]
 
-        snake(block_size, snakelist)
+        # drawing snake
+        draw_snake(block_size, snakelist)
 
         # snake hit the apple
         if lead_x + block_size > apple_x and lead_x < apple_x + appleThickness :
@@ -192,6 +267,7 @@ def gameLoop():
     quit()
 
 
+game_intro()
 
 gameLoop()
 
